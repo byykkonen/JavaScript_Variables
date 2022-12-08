46e8e323af4812d9885e887d05b60463625f6d51
byykkonen <118409410+byykkonen@users.noreply.github.com>
2022-12-08 11:57:48 -0600

    Completed exercise.

--- app.js
+++ app.js
@@ -5,6 +5,42 @@ console.log(
 
 // Exercise 1
 
+const firstName = "Rhysand";
+let lastName = "Byykkonen";
+var age = 35;
+
 // Exercise 2
 
+//Cannot not re-assign firstname;
+lastName = "Archeron";
+age = 21;
+
+
+
 // Exercise 3
+
+let language = "JavaScript";
+let createdYear = 1995 ;
+let isCaseSensitive = true;
+
+console.log(`${language} was released in ${createdYear}`);
+// Exercise 4
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = false;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`The shirt is ${price} is it on sale= ${stock}?`);
+
+// Exercise 5
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`I am reading ${title}. I like the author ${author}. I am on page ${bookmark}!!`);
